1859a466f80efd5ef1441b8a82e88f9a2f09bc36
Daniel Ham <danieljham@gmail.com>
2017-07-31 11:50:58 -0700

    create server scaffolding

---
+++ server/index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var parser = require('body-parser');
+var cors = require('cors');
+var path = require('path');
+
+//file dependencies
+var router = require('./routes');
+
+//setting up express
+var PORT = 3000;
+var app = express();
+app.use(express.static(path.join(__dirname, '../client/static')));
+app.use(cors());
+app.use(parser.json());
+app.use(parser.urlencoded({extended: true}));
+
+//set up routes
+//app.use('/api', router);
+
+//set up server
+app.listen(PORT, function(err){
+  if (err) {
+    console.log('error in connecting to server');
+  } else {
+    console.log('connected to server on port ', PORT);
+  }
+});
